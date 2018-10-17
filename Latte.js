13faf1105f691ce043c6c5ff8d254d98c5194b38
katrinagarloff <katrinagarloff@gmail.com>
2018-10-17 17:43:20 -0400

    tests passing new Latte, keys, addFlavor

--- Latte.js
+++ Latte.js
@@ -1,9 +1,9 @@
 class Latte {
-constructor(milk, base) {
-  this.milk = milk;
-  this.base = base;
-  this.flavors = [];
-}
+  constructor(milk, base) {
+    this.milk = milk;
+    this.base = base;
+    this.flavors = [];
+  }
 
 addFlavor(flavor) {
   this.flavors.push(flavor)
--- test/test.js
+++ test/test.js
@@ -1,21 +1,39 @@
-var chai = require('chai');
-var Latte = require('../Latte.js');
+const expect = require('chai').expect;
+const Latte = require('../Latte.js');
 
 // let dirtyChai = new Latte("Whole milk", "Espresso");
 
+
 describe("Latte Creation", function() {
-  // describe("A latte should be initialized with a milk and base", function(){
-  // });
-  // describe("Initialized lattes should be able to have flavors added to them", function(){
-  // });
+let basicLatte;
+
+  before(function(){
+    basicLatte = new Latte("Whole milk", "Espresso");
+  });
+
+  describe("Should be able to create a new latte", function(){
+    it("Should create a new instance of the Latte class", function(){
+      expect(basicLatte).to.be.an.instanceof(Latte);
+    });
+  });
+
+  describe("A latte should have a milk, base, and flavors.", function(){
+
+    it("should have keys of milk, base, and flavors", function(){
+      expect(basicLatte).to.have.all.keys("milk", "base", "flavors");
+    });
+    it("Should have 'Whole milk', 'Espresso', and flavors pointing to an empty array.", function(){
+      expect(basicLatte).to.deep.equal({milk: 'Whole milk', base: 'Espresso', flavors: []});
+    });
+
+  });
+  describe("Lattes should be able to have flavors", function(){
+    beforeEach(function(){
+      basicLatte.addFlavor("Chai")
+    });
+    it("Should be able to add a flavor to a latte.", function() {
+      expect(basicLatte.flavors).to.include("Chai");
+    });
+
+  });
 });
-
-
-  // describe("Making a dirty chai", function() {
-  // it("Milk should be whole milk", function() {
-  //   chai.assert.equal(dirtyChai.milk, "Whole milk")
-  //   });
-  // it("Base should be 'Espresso'", function(){
-  //   chai.assert.equal(dirtyChai.base, "Espresso");
-  // });
-  // });
