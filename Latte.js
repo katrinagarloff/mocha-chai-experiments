827863ccb353eff4dcc5dd6e7d7e24d379085555
katrinagarloff <katrinagarloff@gmail.com>
2018-10-17 14:37:09 -0400

    Made latte class w/ milk and base constructor

---
+++ Latte.js
@@ -0,0 +1,14 @@
+class Latte {
+constructor(milk, base) {
+  this.milk = milk;
+  this.base = base;
+  this.flavors = [];
+}
+
+addFlavor(flavor) {
+  this.flavors.push(flavor)
+}
+
+}
+
+module.exports = Latte;
---
+++ test/test.js
@@ -0,0 +1,21 @@
+var chai = require('chai');
+var Latte = require('../Latte.js');
+
+// let dirtyChai = new Latte("Whole milk", "Espresso");
+
+describe("Latte Creation", function() {
+  // describe("A latte should be initialized with a milk and base", function(){
+  // });
+  // describe("Initialized lattes should be able to have flavors added to them", function(){
+  // });
+});
+
+
+  // describe("Making a dirty chai", function() {
+  // it("Milk should be whole milk", function() {
+  //   chai.assert.equal(dirtyChai.milk, "Whole milk")
+  //   });
+  // it("Base should be 'Espresso'", function(){
+  //   chai.assert.equal(dirtyChai.base, "Espresso");
+  // });
+  // });
